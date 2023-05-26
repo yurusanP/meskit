e8f509efb3c5514b85b8a2195ec8aa9957704161
Jack Xu <yurusanP@gmail.com>
2023-05-26 16:19:20 -0500

    chore: gradle init

---
+++ README.md
@@ -0,0 +1,8 @@
+# meskit
+
+meskit contains mes[^mes] toolkit for the development of solvers for program synthesis problems
+specified in the Semantic-Guided Synthesis ([SemGuS](https://semgus.org)[^sem]) approach.
+
+[^mes]: "mes" is "my" in French. ([Colin Dictionary](https://www.collinsdictionary.com/us/dictionary/french-english/mes))
+
+[^sem]: "mes" is "sem" written backwards. (Inspired by [Messy](https://github.com/SemGuS-git/Semgus-Messy))
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+  idea
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.dokka)
+}
+
+allprojects {
+  group = "org.yurusanp"
+  version = "0.1.0" + "-SNAPSHOT"
+
+  repositories {
+    mavenCentral()
+  }
+}
+
+subprojects {
+  apply {
+    plugin("org.jetbrains.kotlin.jvm")
+    plugin("org.jetbrains.dokka")
+  }
+
+  kotlin {
+    jvmToolchain(17)
+    sourceSets.all {
+      languageSettings {
+        languageVersion = "2.0"
+      }
+    }
+  }
+
+  dependencies {
+    testImplementation(rootProject.libs.kotest.runner.junit5)
+  }
+
+  tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+  }
+}
